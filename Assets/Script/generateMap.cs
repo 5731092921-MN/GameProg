5528560d1e4cccd67cd4f15510ef050857e2e93f
MINTRA NANKONGNAEB <mind_lover_soul@hotmail.com>
2017-05-14 20:54:25 +0700

    update

---
+++ Assets/Script/generateMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for(int i=1;i<=8;i++)
+            for(int j=1;j<=8;j++)  
+                Instantiate(Resources.Load("floor", typeof(GameObject)), new Vector3(i+i/50.0F,0, j+j/50.0F), Quaternion.identity);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
